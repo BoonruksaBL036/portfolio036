19ef62671e2919bc84ed757064810bf5a468f5dd
BoonruksaBL036 <664259036@webmail.npru.ac.th>
2024-10-02 16:57:44 +0700

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+const Header = () => {
+  const menuFunction = () => {
+    const menuBtn = document.getElementById("myNavMenu");
+    if (menuBtn.className === "nav-menu") {
+      menuBtn.className += "responsive";
+    } else {
+      menuBtn.className = "nav-menu";
+    }
+  };
+  return (
+    <nav id="header">
+      <div className="nav-logo">
+        <p className="nav-name">Boonruksa</p>
+        <span>.</span>
+      </div>
+      <div className="nav-menu" id="myNavMenu">
+        <ul className="nav_menu_list">
+          <li className="nav_list">
+            <a href="#home" className="nav-link active-link">
+              Home
+            </a>
+          </li>
+          <li className="nav_list">
+            <a href="#about" className="nav-link active-link">
+              About
+            </a>
+          </li>
+        </ul>
+        <li className="nav-list">
+          <a href="#project" className="nav-link active-link">
+            Project
+          </a>
+        </li>
+        <li className="nav-list">
+          <a href="#contect" className="nav-link active-link">
+            Contact
+          </a>
+        </li>
+      </div>
+      <div className="nav-button">
+        <button className="btn">
+          Download CV <i className="uil uil-import"></i>
+        </button>
+      </div>
+      <div className="nev-menu-btn">
+        <i className="uil uil-bars" onClick={menuFunction}></i>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
